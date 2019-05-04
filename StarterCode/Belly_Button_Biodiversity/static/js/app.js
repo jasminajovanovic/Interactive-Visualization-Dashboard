82f5c0858e02f0c1855a695f1815f1925f890215
Jasmina Jovanovic <jasminajancic@gmail.com>
2019-05-04 08:33:30 -0500

    working version running locally

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -15,16 +15,78 @@ function buildMetadata(sample) {
     // buildGauge(data.WFREQ);
 }
 
-function buildCharts(sample) {
+function unpack(rows, index) {
+    return rows.map(function(row) {
+        return row[index];
+    });
+}
+
+async function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  sample = await d3.json(`/samples/${sample}`)
+  // @TODO: Build a Bubble Chart using the sample data
+  // @TODO: Build a Pie Chart
+  // create an array of objects for easier sorting
+  let sampleList = []
 
-    // @TODO: Build a Bubble Chart using the sample data
+  for (i=0; i<sample.sample_values.length; i++) {
+    sampleObject = {
+      "otu_ids": sample.otu_ids[i],
+      "otu_labels": sample.otu_labels[i],
+      "sample_values": sample.sample_values[i]
+    }
+    sampleList.push(sampleObject)
+  }
+  // TODO: figure out how this list is printed sorted before the sort is called!
+  // console.log(sampleList)
+  sampleList = sampleList.sort(function(a,b) {
+    return b.sample_values - a.sample_values
+  })
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
+  plotSampleData = sampleList.slice(0, 10)
+  console.log(plotSampleData)
+  otu_ids =  unpack(plotSampleData, "otu_ids")
+  otu_labels = unpack(plotSampleData, "otu_labels")
+  sample_values =  unpack(plotSampleData, "sample_values")
+  console.log(otu_labels)
+  // console.log(sampleList)
+  const trace1 = {
+    "values": sample_values,
+    "labels": otu_ids,
+    "type": "pie",
+    // "text": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // "text": otu_labels
+  }
+  data1 = [trace1]
+  Plotly.newPlot("pie", data1);
+  const trace2 = {
+    x: otu_ids,
+    y: sample_values,
+    mode: 'markers',
+    marker: {
+      size: sample_values
+    }
+    // text: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // hoverinfo = 'text'
+  }
+
+  // TODO: why doesn't `${sample}` work for the graph title
+  const layout2 = {
+    title: "My title",
+    xaxis: {
+      title: {
+        text: 'OTU ID',
+      }
+    }
+  }
+
+  // TODO: figure out hover text in both graphs
+
+  data2 = [trace2]
+  Plotly.newPlot("bubble", data2, layout2)
 }
+
 
 function init() {
   // Grab a reference to the dropdown select element

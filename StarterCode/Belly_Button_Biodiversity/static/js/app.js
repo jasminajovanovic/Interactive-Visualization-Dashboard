0652f5bf0a023f6400af2e603df3af796c6deebc
Jasmina Jovanovic <jasminajancic@gmail.com>
2019-05-06 14:28:51 -0500

    added metadata panel and gauge, correct hoverinfo for pie chart

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,18 +1,107 @@
-function buildMetadata(sample) {
+async function buildMetadata(sample) {
 
   // @TODO: Complete the following function that builds the metadata panel
 
-  // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
+    const thisMetadata = await d3.json(`/metadata/${sample}`)
+    const metadataPanel = d3.select ("#sample-metadata")
 
     // Use `.html("") to clear any existing metadata
+    metadataPanel.html("")
 
+    // TODO: refactor to use object.entries
     // Use `Object.entries` to add each key and value pair to the panel
+    for (key in thisMetadata) {
+      currentElement = metadataPanel.append(`${key}`)
+      currentElement.html(`${key}: ${thisMetadata[key]}<br>`)
+    }
+
+    // htmlString = ""
+
+    // for (key in thisMetadata) {
+    //   htmlString += `${key}: ${thisMetadata[key]} <br>`
+    // }
+
+
+    // metadataPanel.html(htmlString)
+
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
 
+
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+    buildGauge(thisMetadata["WFREQ"])
+}
+
+function buildGauge(level){
+  const gauge = d3.select ("#gauge")
+  // Enter a speed between 0 and 180
+  var newLevel = level*180/8-10;
+
+  // Trig to calc meter point
+  var degrees = 115, radius = .6;
+  var radians = degrees * Math.PI / 180;
+  var x = -1 * radius * Math.cos(radians);
+  var y = radius * Math.sin(radians);
+
+  var degrees = 180 - newLevel,
+       radius = .5;
+  var radians = degrees * Math.PI / 180;
+  var x = radius * Math.cos(radians);
+  var y = radius * Math.sin(radians);
+
+  // Path: may have to change to create a better triangle
+  var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+       pathX = String(x),
+       space = ' ',
+       pathY = String(y),
+       pathEnd = ' Z';
+  var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+  var data = [{ type: 'scatter',
+     x: [0], y:[0],
+      marker: {size: 28, color:'850000'},
+      showlegend: false,
+      name: 'speed',
+      text: level,
+      hoverinfo: 'text+name'},
+    { values: [ 50/8, 50/8, 50/8, 50/8, 50/8, 50/8, 50/8, 50/8, 50],
+    rotation: 90,
+    text: ['>7', '7', '6', '5', '4',
+              '3', '2', '1'],
+    textinfo: 'text',
+    textposition:'inside',
+    marker: {colors:['rgba(0, 80, 0, .5)', 'rgba(4, 107, 0, .5)', 'rgba(24, 137, 10, .5)', 'rgba(120, 164, 32, .5)',
+                           'rgba(180, 212, 52, .5)', 'rgba(212, 219, 105, .5)',
+                           'rgba(220, 216, 155, .5)', 'rgba(242, 236, 212, .5)',
+                           'rgba(255, 255, 255, 0)', 'rgba(265, 265, 265, 0)', 'white']},
+    labels: ['151-180', '121-150', '91-120', '61-90', '31-60', '0-30', ''],
+    hoverinfo: 'label',
+    hole: .5,
+    type: 'pie',
+    showlegend: false
+  }];
+
+  var layout = {
+    shapes:[{
+        type: 'path',
+        path: path,
+        fillcolor: '850000',
+        line: {
+          color: '850000'
+        }
+      }],
+    title: '<b>Gauge</b> <br> Washing Frequency',
+    height: 600,
+    width: 600,
+    xaxis: {zeroline:false, showticklabels:false,
+               showgrid: false, range: [-1, 1]},
+    yaxis: {zeroline:false, showticklabels:false,
+               showgrid: false, range: [-1, 1]}
+  };
+
+  Plotly.newPlot('gauge', data, layout);
 }
 
 function unpack(rows, index) {
@@ -24,9 +113,7 @@ function unpack(rows, index) {
 async function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  thisSample = await d3.json(`/samples/${sample}`)
-  // @TODO: Build a Bubble Chart using the sample data
-  // @TODO: Build a Pie Chart
+  const thisSample = await d3.json(`/samples/${sample}`)
   // create an array of objects for easier sorting
   let sampleList = []
 
@@ -38,30 +125,29 @@ async function buildCharts(sample) {
     }
     sampleList.push(sampleObject)
   }
-  // TODO: figure out how this list is printed sorted before the sort is called!
   sampleList = sampleList.sort(function(a,b) {
     return b.sample_values - a.sample_values
   })
 
   plotSampleData = sampleList.slice(0, 10)
-  // console.log(plotSampleData)
   otu_ids =  unpack(sampleList, "otu_ids")
   otu_labels = unpack(sampleList, "otu_labels")
-  sample_values =  unpack(sampleList, "sample_values")
-  // console.log(otu_labels)
+  sample_values = unpack(sampleList, "sample_values")
 
   const trace1 = {
     values: sample_values.slice(0,10),
     labels: otu_ids.slice(0,10),
+    names: otu_labels.slice(0,10),
     type: "pie",
-    hoverinfo: otu_labels.slice(0,10),
-    mode: 'markers'
-    // "text": otu_labels,
-    // hoverinfo: otu_labels
+    mode: 'markers',
+    hovertext: otu_labels.slice(0,10),
+    hoverinfo: 'label+percent+text',
   }
+  // display the pie chart
   data1 = [trace1]
   Plotly.newPlot("pie", data1);
 
+  // create bubble chart
   const trace2 = {
     x: otu_ids,
     y: sample_values,
@@ -71,9 +157,7 @@ async function buildCharts(sample) {
       color: otu_ids,
       colorscale: "Earth"
     },
-    // text: otu_labels,
-    hoverinfo: otu_labels
-
+    text: otu_labels
   }
   const layout2 = {
     title: `Sample #${sample}`,
@@ -81,12 +165,13 @@ async function buildCharts(sample) {
       title: {
         text: 'OTU ID'
       }
-    }
+    },
+    height: 800,
+    width: 1600
   }
 
-  // TODO: figure out hover text in both graphs
-
   data2 = [trace2]
+  // display the bubble chart
   Plotly.newPlot("bubble", data2, layout2)
 }
 
